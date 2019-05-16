4bc38c8bc2975fb713e89c89646f206c4d13f7f6
Mr__Brown <kbrown@stanwood.wednet.edu>
2019-05-15 17:29:22 -0700

    planet rotation

---
+++ Example game project/Assets/planetRotation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class planetRotation : MonoBehaviour {
+
+    public GameObject planet;// = new GameObject();
+    public float num = 0;
+	// Use this for initialization
+	void Start () {
+        Debug.Log("whats up");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        planet.transform.Rotate(0, 180*Time.deltaTime, 0, Space.Self);
+   }
+}
