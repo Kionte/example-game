b5960b1d80525efc76ed884b00c089aac710509b
Mr__Brown <kbrown@stanwood.wednet.edu>
2019-05-17 13:56:49 -0700

    working on animal kingdom

---
+++ Example game project/Assets/Scripts/kingdom/animal.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animal : MonoBehaviour {
+
+    private bool alive;
+
+    public animal()
+    {
+        alive = true;
+    }
+
+    public bool Alive
+    {
+        get { return alive; }
+        set { alive = value; }
+    }
+ 
+
+
+}
---
+++ Example game project/Assets/Scripts/kingdom/animalphylum/chordata.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class chordata : animal {
+
+    public chordata() { }
+    
+}
